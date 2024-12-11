948ce4041f5216b1d67cd2e79edfa685a43b2b37
jeerawitch <jeerawitch2115@gmail.com>
2024-12-11 22:43:33 +0700

    update

--- Model/connect.php
+++ Model/connect.php
@@ -2,16 +2,30 @@
 require_once __DIR__ . "/../Controller/userController.php";
 require_once __DIR__ . "/../Controller/adminController.php";
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$db = "bookshop";
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8";
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "bookshop";
+// $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 
-try{
+// try{
+//     $pdo = new PDO($dsn, $username, $password);
+//     echo "Connect Database";
+// }catch(PDOException $e){
+//     echo $e->getMessage();
+// }
+
+$host = "fdb1030.awardspace.net"; 
+$username = "4544452_database";
+$password = "6708600_bookshop"; 
+$db = "4544452_database";
+$port = 3306;
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8";
+
+try {
     $pdo = new PDO($dsn, $username, $password);
-    // echo "Connect Database";
-}catch(PDOException $e){
+    // echo "Connected successfully";
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
 
