3f07bea87e9aa6a7459ed1678169fc1aa28aeaef
jeerawitch <jeerawitch2115@gmail.com>
2024-12-12 16:17:38 +0700

    update

--- Model/connect.php
+++ Model/connect.php
@@ -10,7 +10,7 @@ $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 
 try{
     $pdo = new PDO($dsn, $username, $password);
-    echo "Connect Database";
+    // echo "Connect Database";
 }catch(PDOException $e){
     echo $e->getMessage();
 }
