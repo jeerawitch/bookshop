13fbb5f8e6524e5d49f9433df967660b8dab8b72
jeerawitch <jeerawitch2115@gmail.com>
2024-12-19 22:31:11 +0700

    change database

--- Model/connect.php
+++ Model/connect.php
@@ -3,9 +3,9 @@ require_once __DIR__ . "/../Controller/userController.php";
 require_once __DIR__ . "/../Controller/adminController.php";
 
 $host = "sql12.freesqldatabase.com";
-$username = "sql12751536";
-$password = "LE2t1AUkcf";
-$db = "sql12751536";
+$username = "sql12753057";
+$password = "3fcfcFaLRz";
+$db = "sql12753057";
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 
 try{
